e3a73b3a4585430e8c6cb8773495b61135106e30
binghambai <17826828410@163.com>
2023-03-21 14:28:21 +0800

    Initial commit

---
+++ src/main/java/com/example/shortchainssystem/ShortChainsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortchainssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortChainsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortChainsSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shortchainssystem/ShortChainsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortchainssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortChainsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
